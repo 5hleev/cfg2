c2fce3df55b93c57dcb010a6877553fef213c091
5hleev <maksimshleev7@gmail.com>
2024-12-05 03:01:01 +0300

    добавлена проверка оставшихся функций

--- test_main.py
+++ test_main.py
@@ -15,3 +15,26 @@ class TestDependencyVisualizer(unittest.TestCase):
         </dependencies></project>"""
         result = parse_maven_dependencies(pom_content)
         self.assertEqual(result, {"org.example:example-artifact": []})
+
+    def test_build_mermaid_graph(self):
+        dependencies = {"org.example:example-artifact": []}
+        result = build_mermaid_graph(dependencies)
+        expected = """graph TD
+    org.example:example-artifact"""
+        self.assertEqual(result.strip(), expected.strip())
+
+    @patch("builtins.open", new_callable=mock_open)
+    def test_save_mermaid_to_file(self, mock_file):
+        mermaid_graph = "graph TD\n    A --> B"
+        temp_file = "test.mmd"
+        save_mermaid_to_file(mermaid_graph, temp_file)
+
+        # Проверяем, что файл открылся для записи
+        mock_file.assert_called_with(temp_file, 'w')
+
+        # Проверяем, что правильный контент был записан в файл
+        mock_file().write.assert_called_with(mermaid_graph)
+
+
+if __name__ == "__main__":
+    unittest.main()
